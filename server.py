7313ff7abf0abdfa5eb596f74ac333d4e172cd99
Faraji Ombonya <farajiombonya@gmail.com>
2024-10-24 10:23:48 +0300

    feat: try connecting to port 80 of all hosts

--- server.py
+++ server.py
@@ -2,19 +2,46 @@
 Network scanner server module.
 """
 
+import ipaddress
 import socket
 
 HOST = ""
-PORT = "50008"
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    sock.listen(1)
-    client_sock, addr = sock.accept()
+PORT = 50009
 
-    with client_sock:
-        print("Connected by", addr)
+
+def start_sever():
+    """Start server and wait for incoming connections."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
+        sock.listen(1)
+
         while True:
-            data = client_sock.recv(1024)
-            if not data: break
-            client_sock.sendall(data)
-            
+            try:
+                client_sock, addr = sock.accept()
+                with client_sock:
+                    print("Connected by", addr)
+                    data = client_sock.recv(1024).decode()
+
+                    subnet = ipaddress.ip_network(data)
+
+                    for ip in subnet.hosts():
+                        scan_ip(ip, 80)
+
+                    if not data: break  
+                    client_sock.sendall(data.encode())
+            except Exception as e:
+                print(e)
+
+
+
+def scan_ip(host: str, port: int) -> None:
+    """Check if a host is up by connecting to port 80."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            sock.connect((str(host), port))
+        except Exception as e:
+            print("ERROR:", e, host, port)
+
+
+if __name__ == "__main__":
+    start_sever()
